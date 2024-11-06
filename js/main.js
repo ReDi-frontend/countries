ed18ca5983c6343ad878ad77b710abc764900903
ReDi-frontend <infinity.evaluation06@gmail.com>
2024-11-06 13:10:57 +0500

    add dark mode

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const elSiteThemeToggler = document.querySelector(
+    '.site-header-theme-toggler'
+);
+
+elSiteThemeToggler.addEventListener('click', () => {
+    document.body.classList.toggle  ('dark-mode');
+});
